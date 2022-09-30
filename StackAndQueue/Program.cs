95f31aa96e52cc35767b53fb4d78d50c608db70d
pratiksha-bhange <pratikshabhange101@gmail.com>
2022-10-01 01:02:24 +0530

    [Pratu]ADDED: Ability to creatr a Stack Using Linked List

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -7,6 +7,12 @@ namespace StackAndQueue
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stacks and Queues Programs");
+            
+            StackUsingLinkList stackUsingLinkList = new StackUsingLinkList();
+            stackUsingLinkList.push(70);
+            stackUsingLinkList.push(30);
+            stackUsingLinkList.push(56);
+            stackUsingLinkList.Display();
         }
     }
 }
---
+++ StackAndQueue/StackUsingLinkList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackAndQueue
+{
+    class StackUsingLinkList
+    {
+        public Node top;
+        public StackUsingLinkList()
+        {
+            this.top = null;
+        }
+        internal void push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine(value);
+        }
+        internal void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine("Inserted into the stack Successfully = " + temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+}
+    
+
